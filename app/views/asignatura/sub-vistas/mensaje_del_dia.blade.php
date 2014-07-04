82e3de46857fcb7734dc58926399557950e50b04
Alejandro Silva <pm5k.sk@gmail.com>
2014-07-04 03:45:46 -0400

    Close #18, mensajes destacados(mensajes del dia), muestra documentos adjuntos

--- app/views/asignatura/sub-vistas/mensaje_del_dia.blade.php
+++ app/views/asignatura/sub-vistas/mensaje_del_dia.blade.php
@@ -8,4 +8,16 @@
         @yield('opciones-publicacion-destacada')
     </div>
     <p>{{ $publicacion->mensaje }}</p>
+    {{-- hack feo para asignar variables dentro de blade: --}}
+    {{-- */   $documentos = $publicacion->documentos()->get()  /* --}}
+    <div class="row">
+        <div class="tabla">
+            @foreach( $documentos as $documento )
+            <div class="tabla-row">
+                <p class="nombre-doc">{{ $documento->nombre }}</p><a href="#" class="link-doc">
+                    <a href="{{ URL::to($documento->url) }}" target="_blank"><span class="glyphicon glyphicon-cloud-download">descargar</span></a>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </article>
